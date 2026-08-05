d63f5894df919db657cf9cde98172f659f5beb58
Aswandha <aswandharj@gmail.com>
2026-08-05 19:48:40 +0530

    Update database configuration for production

--- config/db.js
+++ config/db.js
@@ -1,14 +1,13 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const connectDB = async () => {
   try {
-    const connStr = process.env.MONGO_URI || 'mongodb://localhost:27017/onam_luckydraw';
-    const conn = await mongoose.connect(connStr);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
-    return conn;
   } catch (error) {
     console.error(`MongoDB Connection Error: ${error.message}`);
     process.exit(1);
